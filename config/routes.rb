8b4adfab7e54061130a3c65c38c96645a59c7d2a
Ehab Elia <ehab.elia@gmail.com>
2015-11-10 10:52:31 +0100

    routes conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  # root to: 'pages#home'
+
+   devise_for :users
   root to: 'pages#home'
+
+  resources :dossiers
+
+  resources :candidates do
+    resources :proofs
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
