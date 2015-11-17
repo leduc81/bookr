ba9c5b6c3f544902d7894054606261f5fa5a0b77
Maxime Urien <maxime.urien@gmail.com>
2015-11-17 17:03:48 +0100

    token auth iter2

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,6 +1,9 @@
 class GuestsController < ApplicationController
-  before_action :def_guest, only: [:show]
-  before_action :def_user, only: [:show]
+
+  before_action :def_user_guest_by_token, only: [:show]
+  before_action :def_user, only: [:new, :update]
+  before_action :def_guest, only: [:update]
+
   skip_before_action :authenticate_user!
 
   def show
@@ -9,23 +12,69 @@ class GuestsController < ApplicationController
     else
       render text: "You don't have the permission to access this ressource"
     end
+  end
 
+  def update
+    @guest.canceled = true
+    @guest.save
+    redirect_to dashboard_share_path
+  end
+
+  def new
+    @guest = Guest.new
+  end
+
+  def create
+    days_last = params['guest']['days'].to_i
+    email = params['guest']['email'].nil? ? "" : params['guest']['email']
+    start = Time.now
+    expires = start + days_last.days
+    guest = Guest.create(
+      user_id: current_user.id,
+      token: generate_token,
+      email: email,
+      start: start,
+      expires: expires,
+      viewed_t: 0,
+      dl_t: 0
+    )
+    redirect_to dashboard_share_path
   end
 
   private
 
-  def def_guest
+  def def_user_guest_by_token
     @guest = Guest.where(token: params['id']).first
+    if @guest
+      if @guest.canceled == true
+        @guest = nil
+      else
+        @user = @guest.user
+      end
+    end
+  end
+
+
+  def def_guest
+    @guest = Guest.find(params['id'])
   end
 
   def def_user
-    @user = @guest.user if @guest
+    @user = User.find(params['user_id'])
   end
 
   def increment_viewed_counter
       @guest.viewed_t = 0 unless @guest.viewed_t
       @guest.viewed_t = @guest.viewed_t + 1
       @guest.save
+  end
+
+  def generate_token
+    letters = (0...20).map { ('a'..'z').to_a[rand(26)] }.join
+    numbers = (0...20).map { (0..9).to_a[rand(10)] }.join
+    special = (0...10).map { ["=", "+"].to_a[rand(2)] }.join
+    string = letters + numbers + special
+    string.chars.shuffle.join
   end
 
 end
