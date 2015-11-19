f54358c7be1c0c25581ea7227565b553c2d000ea
Pedro Meyer <oduque@me.com>
2015-11-19 12:33:54 +0100

    paperclip working and pictures too

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -2,7 +2,7 @@ class Candidate < ActiveRecord::Base
   belongs_to :user
   has_many :proofs
   has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "150x150>" }
+    styles: { medium: "300x300#", thumb: "150x150#" }
     validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
 
